5b394c17ff6fe6fbe6e50d3df625022cd25bf074
suresurya <suresrivenkatramasurya@gmail.com>
2026-04-03 14:16:58 +0530

    spring modifications

---
+++ sureprompt-web/src/main/java/com/sureprompt/config/FlywayCleanConfig.java
@@ -0,0 +1,18 @@
+package com.sureprompt.config;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FlywayCleanConfig {
+
+    @Bean
+    public FlywayMigrationStrategy cleanMigrateStrategy() {
+        return flyway -> {
+            flyway.clean();
+            flyway.migrate();
+        };
+    }
+}
